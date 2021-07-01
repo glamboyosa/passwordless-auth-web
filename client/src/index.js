2bbd79d87a76d82f8ab81e78f26918665f402b6f
glamboyosa <ogbemudiatimothy@gmail.com>
2021-07-01 13:41:08 +0100

    chore: added variable imports

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import Toastify from 'toastify-js'
 
 import 'toastify-js/src/toastify.css'
 
+import { form, phoneNumber } from './helpers/variables'
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
 })
