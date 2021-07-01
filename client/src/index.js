0ccdec843b2d5be137fed90ff4c899009422dbc3
Phil Leggetter <phil@leggetter.co.uk>
2021-07-01 18:28:08 +0100

    chore: remove most of the code from client/src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,1 @@
 import 'regenerator-runtime/runtime'
-
-import Toastify from 'toastify-js'
-
-import 'toastify-js/src/toastify.css'
-
-import { formElement } from './helpers/variables'
-
-formElement.addEventListener('submit', async (e) => {
-  e.preventDefault()
-})
